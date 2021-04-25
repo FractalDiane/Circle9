0faae840f8e1dc6a6a1115b2e42120bdf90b962e
Diane Sparks <duncan.sprks@gmail.com>
2021-04-25 11:26:51 -0700

    Lots more stuff

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -3,6 +3,13 @@ extends Node
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+		
+		
+func change_scene(scene: String, position: Vector3) -> void:
+	get_tree().change_scene(scene)
+	yield(get_tree(), "idle_frame")
+	if position != Vector3.ZERO:
+		get_tree().current_scene.get_node("Player").set_translation(position)
 
 
 func play_intro_music() -> void:
--- scripts/door.gd
+++ scripts/door.gd
@@ -4,6 +4,8 @@ signal opened()
 
 export(String, FILE, "*.tscn") var target_scene: String
 export(AudioStream) var door_sound: AudioStream
+export(String) var interact_text := "Enter"
+export(Vector3) var target_position := Vector3.ZERO
 
 var in_area := false
 var player_ref = null
@@ -15,7 +17,7 @@ func _process(_delta: float) -> void:
 		emit_signal("opened")
 		$SoundDoor.set_stream(door_sound)
 		$SoundDoor.play()
-		PlayerUI.show_interact(false, "Enter")
+		PlayerUI.show_interact(false, interact_text)
 		if player_ref != null:
 			player_ref.set_allow_input(false)
 		
@@ -27,16 +29,17 @@ func _process(_delta: float) -> void:
 func _on_Door_body_entered(body: Node) -> void:
 	in_area = true
 	player_ref = body
-	PlayerUI.show_interact(true, "Enter")
+	PlayerUI.show_interact(true, interact_text)
 
 
 func _on_Door_body_exited(_body: Node) -> void:
 	in_area = false
 	player_ref = null
 	if not teleporting:
-		PlayerUI.show_interact(false, "Enter")
+		PlayerUI.show_interact(false, interact_text)
 
 
 func _on_TimerTeleport_timeout() -> void:
-	get_tree().change_scene(target_scene)
+	#get_tree().change_scene(target_scene)
+	Controller.change_scene(target_scene, target_position)
 	PlayerUI.fadeout(false)
